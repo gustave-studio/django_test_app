76780c64db825ac64a9ce0bdcfa3dbdf1980663c
gustave-studio <gustave0222@gmail.com>
2023-07-17 06:08:52 +0900

    アプリとプロジェクトの繋ぎ

--- helloworldproject/helloworldapp/urls.py
+++ helloworldproject/helloworldapp/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from .views import helloworldfunction, HelloWorldClass
+from django.urls import path
+from .views import helloworldappview
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('helloworld/', helloworldfunction),
-    path('helloworld2/', HelloWorldClass.as_view()),
-    path('', include('helloworldapp.urls')),
+    path('helloworldapp/', helloworldappview)
 ]
